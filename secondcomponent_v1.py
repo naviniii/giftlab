43878a5c386506859146b67802d1a12d3a9847a8
naviniii <navini.ariyasinghe@gmail.com>
2023-06-19 10:25:50 +1200

    Create secondcomponent_v1.py
    created second component (this is the first version)
    
    added questions, but will do some research on questions first

---
+++ secondcomponent_v1.py
@@ -0,0 +1,38 @@
+#this will be the personality quiz part of my program
+
+
+
+from tkinter import*
+
+from tkinter import messagebox
+
+widnow = Tk()
+
+window.title("Giftlab")
+window.geometry("500x500")
+
+
+
+#creating the questions and answers lists
+
+#this personality quiz will have three options (a, b, c)
+#i will use radio buttons to achieve this
+
+
+q = [
+    "Question 1: \n ",
+    "Question 2: \n",
+    "Question 3: \n",
+    "Question 4: \n",
+    "Question 5: \n",
+    "Question 6: \n",
+    "Question 7: \n",
+    "Question 8: \n",
+    "Question 9: \n",
+    "Question 10: \n",
+
+    ]
+
+    
+options = [
+    "
